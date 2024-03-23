499f32a2e5b79dc6681ab4941b8c64e989a1da3a
jwa334 <jwa334@sfu.ca>
2024-03-23 15:12:19 -0700

    tailwindcss setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        'light-gray': '#f3f3f6',
+      }
+    },
+  },
+  plugins: [],
+}
+
